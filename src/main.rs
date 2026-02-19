cec987ac8015616a3516bc30e273f2d840555428
nayraarr <zita.nayra.ardini@gmail.com>
2026-02-19 18:29:51 +0700

    Create database dummy calling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod db;
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL")
+        .expect("DATABASE_URL harus diset di .env");
+
+    let pool = db::create_pool(&database_url).await;
+    println!("Berhasil konek ke Neon DB!");
+
+    // Test koneksi dengan ping — tidak butuh tabel apapun
+    sqlx::query("SELECT 1")
+        .execute(&pool)
+        .await
+        .expect("Gagal ping database");
+
+    println!("Koneksi ke DB modul 3 berjalan normal!");
 }
