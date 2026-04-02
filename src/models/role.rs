f73c6c22225b152ddace9318a689a26f405535b6
nayraarr <zita.nayra.ardini@gmail.com>
2026-04-02 16:52:07 +0700

    Apply str version of role enum.

--- src/models/role.rs
+++ src/models/role.rs
@@ -1,7 +1,9 @@
+use std::fmt;
+use std::fmt::Display;
+use std::str::FromStr;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-// ENUM UNTUK ROLE
 #[derive(Debug, Serialize, Deserialize, sqlx::Type, Clone, PartialEq, ToSchema)]
 #[sqlx(type_name = "order_status", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum Role {
@@ -9,4 +11,35 @@ pub enum Role {
     Jastiper,
     Admin,
     System,
+}
+
+impl Role {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Role::Titipers => "TITIPERS" ,
+            Role::Jastiper => "JASTIPER",
+            Role::Admin => "ADMIN",
+            Role::System => "SYSTEM",
+        }
+    }
+}
+
+impl FromStr for Role {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "TITPERS" => Ok(Role::Titipers),
+            "JASTIPER" => Ok(Role::Jastiper),
+            "ADMIN" => Ok(Role::Admin),
+            "SYSTEM" => Ok(Role::System),
+            _ => Err(format!("Nilai Role tidak valid: '{}'", s)),
+        }
+    }
+}
+
+impl Display for Role {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
 }
