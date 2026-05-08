c3690c309b8af8a260f8a8d483f3de066fb84a90
nayraarr <zita.nayra.ardini@gmail.com>
2026-05-08 17:15:18 +0700

    Update tests.

--- src/tests/unit/services/mod.rs
+++ src/tests/unit/services/mod.rs
@@ -1,5 +1,5 @@
-pub mod http_client_test;
-pub mod order_internal_test;
-pub mod order_test;
+pub mod http_client_service_test;
+pub mod order_internal_service_test;
+pub mod order_service_test;
 pub mod rating_jastiper_service_test;
 pub mod rating_product_service_test;
