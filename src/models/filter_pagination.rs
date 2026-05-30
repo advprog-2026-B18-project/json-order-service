3a986d89b904529782bf09219a2c5179c803b0e9
nayraarr <zita.nayra.ardini@gmail.com>
2026-05-30 21:21:08 +0700

    Fix params.

--- src/models/filter_pagination.rs
+++ src/models/filter_pagination.rs
@@ -1,8 +1,20 @@
 use crate::models::order_state::OrderStatus;
 use chrono::{DateTime, Utc};
+use serde::de;
 use serde::Deserialize;
 use utoipa::ToSchema;
 use uuid::Uuid;
+
+fn opt_i64_from_str<'de, D>(d: D) -> Result<Option<i64>, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    match Option::<String>::deserialize(d)? {
+        None => Ok(None),
+        Some(ref s) if s.is_empty() => Ok(None),
+        Some(s) => s.parse().map(Some).map_err(de::Error::custom),
+    }
+}
 
 #[derive(Debug, Deserialize, ToSchema, Default)]
 pub struct OrderFilter {
@@ -16,7 +28,9 @@ pub struct OrderFilter {
 
 #[derive(Debug, Deserialize, ToSchema, Default)]
 pub struct PaginationParams {
+    #[serde(default, deserialize_with = "opt_i64_from_str")]
     pub page: Option<i64>,
+    #[serde(default, deserialize_with = "opt_i64_from_str")]
     pub limit: Option<i64>,
     pub sort_by: Option<String>,
     pub order: Option<SortOrder>,
